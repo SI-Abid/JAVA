ef3e129aeaeaaeacc4e69a6b74534e947966c7a6
saiham <siaabid5323@gmail.com>
2021-12-03 00:01:30 +0600

    simple calculator

--- Classworks/Dec01/Swing/src/SimpleCalculator.java
+++ Classworks/Dec01/Swing/src/SimpleCalculator.java
@@ -5,39 +5,52 @@ public class SimpleCalculator {
         
         JFrame frame = new JFrame("Simple Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 200);
+        frame.setSize(300, 300);
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-
-        JPanel panel = new JPanel();
-        frame.add(panel);
-
+        frame.setLayout(null);
+        
         JLabel label = new JLabel("Enter two numbers");
-        label.setBounds(150,10,200,20);
-        panel.add(label);
-
-        JTextField textField1 = new JTextField(10);
-        panel.add(textField1);
-
-        JTextField textField2 = new JTextField(10);
-        panel.add(textField2);
-
-        JButton button = new JButton("Calculate");
-        panel.add(button);
-
-        JLabel resultLabel = new JLabel("Result");
-        panel.add(resultLabel);
-
-        JTextField resultField = new JTextField(10);
-        panel.add(resultField);
-
+        label.setBounds(85, 10, 200, 30);
+        frame.add(label);
+        
+        JTextField textField1 = new JTextField();
+        textField1.setBounds(35, 50, 100, 30);
+        frame.add(textField1);
+        
+        JTextField textField2 = new JTextField();
+        textField2.setBounds(155, 50, 100, 30);
+        frame.add(textField2);
+        
+        JButton button = new JButton("Add");
+        button.setBounds(35, 100, 100, 30);
+        frame.add(button);
+        
+        JButton button2 = new JButton("Subtract");
+        button2.setBounds(155, 100, 100, 30);
+        frame.add(button2);
+        
+        JLabel label2 = new JLabel("Result");
+        label2.setBounds(50, 150, 100, 30);
+        frame.add(label2);
+        
+        JTextField textField3 = new JTextField();
+        textField3.setBounds(110, 150, 100, 30);
+        frame.add(textField3);
+        
         button.addActionListener(e -> {
-            String text1 = textField1.getText();
-            String text2 = textField2.getText();
-            int number1 = Integer.parseInt(text1);
-            int number2 = Integer.parseInt(text2);
-            int result = number1 + number2;
-            resultField.setText(String.valueOf(result));
+            int num1 = Integer.parseInt(textField1.getText());
+            int num2 = Integer.parseInt(textField2.getText());
+            int result = num1 + num2;
+            textField3.setText(String.valueOf(result));
         });
+
+        button2.addActionListener(e -> {
+            int num1 = Integer.parseInt(textField1.getText());
+            int num2 = Integer.parseInt(textField2.getText());
+            int result = num1 - num2;
+            textField3.setText(String.valueOf(result));
+        });
+
+        frame.setVisible(true);
     }
 }
