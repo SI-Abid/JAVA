1cd44ee971eddb0beae27ead057cf26d868103e6
saiham <siaabid5323@gmail.com>
2021-12-05 19:02:12 +0600

    updated

--- Classworks/Dec01/Swing/src/Container.java
+++ Classworks/Dec01/Swing/src/Container.java
@@ -2,16 +2,22 @@ import javax.swing.*;
 
 public class Container {
     public static void main(String[] args) {
+        
         JFrame frame = new JFrame("Leading University");
+
         JLabel label = new JLabel("Name");
         label.setBounds(10, 10, 80, 25);
+
         JTextField text = new JTextField("Enter Name");
         text.setBounds(100, 10, 160, 25);
+
         JButton button = new JButton("DONE");
         button.setBounds(10, 40, 80, 25);
+
         frame.add(label);
         frame.add(text);
         frame.add(button);
+
         frame.setSize(300, 300);
         frame.setLayout(null);
         frame.setVisible(true);
