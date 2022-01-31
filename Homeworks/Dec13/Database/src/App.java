5f30ec579bf9562d252b2f1c3a61865c7737217a
saiham <siaabid5323@gmail.com>
2022-01-31 20:06:30 +0600

    delete feature added

--- Homeworks/Dec13/Database/src/App.java
+++ Homeworks/Dec13/Database/src/App.java
@@ -68,7 +68,8 @@ public class App {
                     if(name.equals("") || age.equals("")) {
                         JOptionPane.showMessageDialog(null, "Please fill in all fields");
                     } else {
-                        stmt2.executeUpdate("INSERT INTO students (name, age) VALUES ('" + name + "', " + age + ")");
+                        String query = String.format("INSERT INTO students (name, age) VALUES ('%s', %s)", name, age);
+                        stmt2.executeUpdate(query);
                         model.addRow(new Object[] {name, age});
                         t1.setText("");
                         t2.setText("");
@@ -85,46 +86,18 @@ public class App {
             // add action listener to table
             table.addMouseListener(new MouseAdapter(){
                 public void mouseClicked(MouseEvent e){
-                    // if(e.getButton()==MouseEvent.BUTTON1) {
 
-                    //     int row = table.getSelectedRow();
-                    //     // delete this row
-                    //     String name = table.getValueAt(row, 0).toString();
-                    //     String age = table.getValueAt(row, 1).toString();
-                    //     int age2 = Integer.parseInt(age);
-                    //     try {
-                    //         Connection con3 = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
-                    //         Statement stmt3 = con3.createStatement();
-                    //         stmt3.executeUpdate("DELETE FROM students WHERE name = '" + name + "' AND age = " + age2);
-                    //         model.removeRow(row);
-                    //         JOptionPane.showMessageDialog(null, "User deleted");
-                    //         con3.close();
-                    //     } catch (Exception ex) {
-                    //         ex.printStackTrace();
-                    //     }
-                    // }
-                    // if click button is right click
-
-                    if(e.getButton()==MouseEvent.BUTTON1) {
-                        int row = table.getSelectedRow();
-                        // edit this row
-                        String name = table.getValueAt(row, 0).toString();
-                        String age = table.getValueAt(row, 1).toString();
-                        int age2 = Integer.parseInt(age);
-                        String newName = JOptionPane.showInputDialog(null, "Name: " + name);
-                        int newAge = Integer.parseInt(JOptionPane.showInputDialog(null, "Age: " + age));
-
-                        try {
-                            Connection con3 = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
-                            Statement stmt3 = con3.createStatement();
-                            stmt3.executeUpdate("UPDATE students SET name = '" + newName + "', age = " + newAge + " WHERE name = '" + name + "' AND age = " + age2);
-                            JOptionPane.showMessageDialog(null, "User updated");
-                            con3.close();
-                            table.setValueAt(newName, row, 0);
-                            table.setValueAt(newAge, row, 1);
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                        }
+                    if(e.getButton()==MouseEvent.BUTTON3) {
+                        // pop up menu for delete and edit
+                        JPopupMenu popup = new JPopupMenu();
+                        JMenuItem delete = new JMenuItem("Delete");
+                        JMenuItem edit = new JMenuItem("Edit");
+                        popup.add(delete);
+                        popup.add(edit);
+                        popup.show(table, e.getX(), e.getY());
+                        int row = table.rowAtPoint(e.getPoint());
+                        delete.addActionListener(l -> delete(row, table, model));
+                        edit.addActionListener(l -> update(row, table));
                     }
                 }
             });
@@ -136,5 +109,43 @@ public class App {
             System.out.println(e);
         }
         
+    }
+    public static void update(int row, JTable table) {
+        // int row = table.getSelectedRow();
+        // edit this row
+        String name = table.getValueAt(row, 0).toString();
+        String age = table.getValueAt(row, 1).toString();
+        int age2 = Integer.parseInt(age);
+        String newName = JOptionPane.showInputDialog(null, "Name: " + name);
+        int newAge = Integer.parseInt(JOptionPane.showInputDialog(null, "Age: " + age));
+
+        try {
+            Connection con3 = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+            Statement stmt3 = con3.createStatement();
+            String query = String.format("UPDATE students SET name = '%s', age = %s WHERE name = '%s' AND age = %s", newName, newAge, name, age2);
+            stmt3.executeUpdate(query);
+            JOptionPane.showMessageDialog(null, "User updated");
+            con3.close();
+            table.setValueAt(newName, row, 0);
+            table.setValueAt(newAge, row, 1);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    public static void delete(int row, JTable table, DefaultTableModel model) {
+        try {
+            Connection con3 = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+            Statement stmt3 = con3.createStatement();
+            String name = table.getValueAt(row, 0).toString();
+            String age = table.getValueAt(row, 1).toString();
+            int age2 = Integer.parseInt(age);
+            String query = String.format("DELETE FROM students WHERE name = '%s' AND age = %s", name, age2);
+            stmt3.executeUpdate(query);
+            JOptionPane.showMessageDialog(null, "User deleted");
+            con3.close();
+            model.removeRow(row);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
